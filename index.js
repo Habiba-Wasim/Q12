bec27ecd9acc148069a6628ace46362d0040767b
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 21:07:21 +0500

    Step12 Of Typescript
    Step12 Of Typescript

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+//12. Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = ["taha", "tayyab", "wasim"];
+let message = "where are you going?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
---
+++ index.ts
@@ -0,0 +1,9 @@
+//12. Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string [] = ["taha", "tayyab", "wasim"];
+
+let message: string = "where are you going?";
+
+console.log(names[0] + " " + message)
+console.log(names[1] + " " + message)
+console.log(names[2] + " " + message)
